63f8f2d814e7a0c5f9c161d461cbccac5e700b48
Daisuke Igarashi <daisuke@dkpyn.com>
2015-11-07 15:13:22 +0900

    Update run scripts and README

--- server/run.sh
+++ server/run.sh
@@ -2,5 +2,14 @@
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 cd $script_dir
-source ../env/bin/activate
+
+if [ -e ../env ]; then
+	source ../env/bin/activate
+fi
+
+if [ ! -e config.py ]; then
+	cp config.py.sample config.py
+	python app.py db upgrade
+fi
+
 python app.py runserver --host=0.0.0.0 --port=9092 --reload
