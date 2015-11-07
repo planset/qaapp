5f1eb623993c3992f5aae57af3b8a28e199c3e4b
Daisuke Igarashi <daisuke@dkpyn.com>
2015-11-07 12:24:25 +0900

    Change to use SQLAlchemy

--- server/app.py
+++ server/app.py
@@ -1,90 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import (Flask, request, session, g, redirect,
-                   url_for, render_template, flash,
-                   send_from_directory, abort,
-                   render_template, flash, send_from_directory,
-                   render_template_string)
 import json
 
-from flask_mysqldb import MySQL
+from flask import (Flask, request, jsonify)
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import BadRequest
+
 import config
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = config.MYSQL_USER
-app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
-app.config['MYSQL_DB'] = config.MYSQL_DB
-app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI;
 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
 
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deleted = db.Column(db.Boolean, nullable=False, default=False)
+    q = db.Column(db.Text, nullable=False, default='')
+    a = db.Column(db.Text, nullable=False, default='')
+    cat = db.Column(db.String(255), nullable=False, default='')
+    sort = db.Column(db.Integer, nullable=False, default=0)
 
-@app.before_request
-def setup_db():
-    app.con = mysql.connection
-    app.cur = mysql.connection.cursor()
+    def __init__(self, q, a):
+        self.deleted = False
+        self.q = q
+        self.a = a
+        self.cat = ''
+        self.sort = 0
+
+    def to_json(self):
+        return dict(id=self.id,
+                deleted=self.deleted,
+                q=self.q,
+                a=self.a,
+                cat=self.cat,
+                sort=self.sort)
+
 
 @app.route('/cards/')
 def list_card():
-    app.cur.execute('''SELECT id, q, a, cat FROM cards''')
-    items = []
-    for item in app.cur.fetchall():
-        items.append({
-            'id': item[0],
-            'q': item[1],
-            'a': item[2],
-            'cat': item[3]
-            })
-    return json.dumps({'result':'ok', 'data':items})
+    cards = Card.query.filter_by(deleted=False).all();
+    return jsonify(result='ok', data=[c.to_json() for c in cards])
 
 @app.route('/cards/<int:id>', methods=['DELETE'])
 def del_card(id):
-    app.cur.execute('''DELETE FROM cards WHERE id = %s;''', (id,))
-    app.con.commit()
-    return json.dumps({'result':'ok', 'data': ''})
+    card = Card.query.get_or_404(id)
+    card.deleted = True
+    db.session.add(card)
+    db.session.commit()
+    return jsonify(result='ok')
 
-def _get_param(req):
-    id = req.form.get('id')
-    q = req.form.get('q')
-    a = req.form.get('a')
-    cat = req.form.get('cat')
-    if id is None and q is None and a is None and cat is None:
-        d = json.loads(req.data)
-        id = d.get('id')
-        q = d.get('q')
-        a = d.get('a')
-        cat = d.get('cat')
-        if id is None and q is None and a is None and cat is None:
-            return None
-    return {
-        'id': id,
-        'q': q,
-        'a': a,
-        'cat': cat
-    }
+def validate_param_card(p):
+    if not p.has_key('q'): raise BadRequest('Param q is required.')
+    if p.get('q') == '': raise BadRequest('Param q is not allow empty.')
+    if not p.has_key('a'): raise BadRequest('Param a is required.')
 
 @app.route('/cards/<int:id>', methods=['PUT'])
 def update_card(id):
-    p = _get_param(request)
-    if p is None:
-        return json.dumps({'result':'ng', 'data': 'Invalid parameters'})
+    p = request.json
+    validate_param_card(p)
 
-    app.cur.execute(
-        '''UPDATE cards SET q = %s, a = %s WHERE id = %s;''',
-        (p['q'], p['a'], id)
-        )
-    app.con.commit()
-    return json.dumps({'result':'ok', 'data': ''})
+    card = Card.query.get_or_404(id)
+    card.q = p['q']
+    card.a = p['a']
+    db.session.add(card)
+    db.session.commit()
+    return jsonify(result='ok')
 
 @app.route('/cards/', methods=['POST'])
 def add_card():
-    p = _get_param(request)
-    if p is None:
-        return json.dumps({'result':'ng', 'data': 'Invalid parameters'})
-    app.cur.execute('''INSERT INTO cards(q, a) values(%s, %s);''', (p['q'], p['a']))
-    app.con.commit()
-    return json.dumps({'result':'ok', 'data': ''})
+    p = request.json
+    validate_param_card(p)
+
+    card = Card(p['q'], p['a'])
+    db.session.add(card)
+    db.session.commit()
+    return jsonify(result='ok')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9092, debug=True)
--- server/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cards (
-	id int(11) AUTO_INCREMENT,
-	q text NOT NULL default '',
-	a text NOT NULL default '',
-	cat varchar(255) NOT NULL default '',
-	sort int(11) NOT NULL default 0,
-	INDEX(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
